1848e6425df5d1c7f082b1a82d3aec0c04588f88
tmullen <tmullen1991@gmail.com>
2018-12-13 21:17:56 -0700

    changed some html stuff and setup click event for save that doesnt do anything yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-    
+    //firebase stuff here
+    $(document).on("click", "#save", function(){
+        // push data to firebase
+        console.log("add save stuff someone :)")
+    })
 })
