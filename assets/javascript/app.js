291a02d3a4d5df9651bf96c49e2ae80134ebd592
tmullen <tmullen1991@gmail.com>
2018-12-15 11:23:42 -0700

    added some javascript and changed save button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    var noteContent
     //firebase stuff here
-    $(document).on("click", "#save", function(){
+    $(document).on("click", "#save", function () {
         // push data to firebase
-        console.log("add save stuff someone :)")
+        noteContent = $("#note-content").val().trim()
     })
+    // appends the notes name to the header, change 'name goes here' to name variable from database 
+    $("#note-name").prepend("<a href='#'' id='note-name' class='brand-logo right'>" + 'name goes here' + "</a>")
+    
 })
