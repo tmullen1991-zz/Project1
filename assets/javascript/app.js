9a5daaad95a6624c64825dd0b50cb4dbc6677cd6
tmullen <tmullen1991@gmail.com>
2018-12-18 17:52:41 -0700

    changed styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,6 @@ $(document).ready(function () {
         noteContent = $(".note-content").val().trim()
     })
     // appends the notes name to the header, change 'name goes here' to name variable from database 
-    $("#note-name").prepend("<a href='#' class='brand-logo note-name right'>" + 'name goes here' + "</a>")
+    $("#note-name").prepend("<a href='#' class='brand-logo note-name right ml-5'>" + 'name goes here' + "</a>")
     
 })
