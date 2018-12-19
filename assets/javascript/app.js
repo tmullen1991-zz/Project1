db9ceaccb120a66c9cd36361659ee85d856db896
tmullen <tmullen1991@gmail.com>
2018-12-18 20:02:04 -0700

    removed edit.css and moved anything needed into the index.css file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,11 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 
+    // needed to allow for materialize input for mobile(see materialize/text-inputs on website)
+    // add the scond line everytime after jQuery change the textarea's val()
+    $('#textarea1').val();
+    M.textareaAutoResize($('#textarea1'));
+
     var noteContent
     $(document).on("click", "#save", function () {
         // push data to firebase
@@ -17,6 +22,6 @@ $(document).ready(function () {
         
     })
     // appends the notes name to the header, change 'name goes here' to name variable from database 
-    $("#note-name").prepend("<a href='#' class='brand-logo note-name right ml-5'>" + 'name goes here' + "</a>")
+    $("#note-name").prepend("<a href='#' class='brand-logo ml-5 center nav nav-text'>" + 'name goes here' + "</a>")
 
 })
